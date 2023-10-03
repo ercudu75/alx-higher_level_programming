989e3cdb174aa6ca304d566998414ca9dad1326e
ilyass <ilyass.bentalha2@gmail.com>
2023-10-03 23:44:42 +0100

    fix problem output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    last_digit = i % 10
-    first_digit = i // 10
-    if first_digit < last_digit:
-        print("{}{}".format(first_digit, last_digit), end=", " if i < 89 else "\n")
+for first in range(0, 9):
+    for last in range(first + 1, 10):
+        if first != 8:
+            print("{}{}".format(first, last), end=", ")
+print("89")
