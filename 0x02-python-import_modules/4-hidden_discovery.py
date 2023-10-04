4b4884710b08d9662da403455728f16e14aabc03
ilyass <ilyass.bentalha2@gmail.com>
2023-10-04 23:05:02 +0100

    pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     import hidden_4 as hidden
 
     for name in dir(hidden):
-        if name[0 : 2] != '__':
-                print(name)
-
-
+        # you can use this condition also "if not name.startswith('__'): "
+        if name[0:2] != '__':
+            print(name)
