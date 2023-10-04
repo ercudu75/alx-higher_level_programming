4cad20794ddb1a2538c745eacf3895c535ced060
ilyass <ilyass.bentalha2@gmail.com>
2023-10-04 21:10:56 +0100

    addd task prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 
 if __name__ == "__main__":
     import sys
-    for i in range(0, len(sys.argv)):
-        if len(sys.argv) - 1 == 0:
-            print("0 arguments.")
-            break
+
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    else:
         print("{} arguments:".format(len(sys.argv) - 1))
-        if len(sys.argv) - 1 > 0 and i >= 1:
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
 
 
