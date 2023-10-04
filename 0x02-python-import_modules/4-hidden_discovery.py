421b9aaa0434f64ca14505b4553eff7f02e933c9
ilyass <ilyass.bentalha2@gmail.com>
2023-10-04 22:56:20 +0100

    pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	import hidden_4 as hidden
+    import sys
+    import hidden_4 as hidden
 
     for name in dir(hidden):
         if name[0 : 2] != '__':
