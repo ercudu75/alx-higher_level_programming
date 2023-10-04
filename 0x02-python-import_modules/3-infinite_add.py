514642a7a91ca0b460b41055a25551ed17cdcc46
ilyass <ilyass.bentalha2@gmail.com>
2023-10-04 21:26:38 +0100

    pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	sum = 0
-	if len(sys.argv) - 1 == 0:
-		print("0")
-	else:
-		for i in range(1, len(sys.argv)):
-			sum += int(sys.argv[i])
-		print("{}".format(sum))
+    import sys
+    sum = 0
+    if len(sys.argv) - 1 == 0:
+        print("0")
+    else:
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
