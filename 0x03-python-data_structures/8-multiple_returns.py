9142d257b5e6864d65e52c9b48b800283bc55951
ilyass <ilyass.bentalha2@gmail.com>
2023-10-09 18:19:20 +0100

    update task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == '':
-        return None
-    tuple_c = len(sentence), sentence[0]
-    return tuple_c
+        return 0, None
+    return len(sentence), sentence[0]
