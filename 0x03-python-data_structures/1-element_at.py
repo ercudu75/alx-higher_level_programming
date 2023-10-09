42aa5b46601e7c0a650301bfc22497991cb2f6d0
ilyass <ilyass.bentalha2@gmail.com>
2023-10-09 12:43:47 +0100

    update task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    if int(idx) < 0 or int(idx) > len(my_list):
+    if int(idx) < 0 or int(idx) >= len(my_list):
         return None
     else:
         return my_list.index(idx)
