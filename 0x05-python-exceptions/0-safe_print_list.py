9169e6e5cf9a4236b18bcb8e5e28ca7cded8131d
ilyass <ilyass.bentalha2@gmail.com>
2023-10-23 16:29:01 +0100

    add task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in my_list:
+            print("{}".format(i), end='')
+            count += 1
+            if count == x:
+                break
+        print()
+    except IndexError:
+        pass
+    return count
