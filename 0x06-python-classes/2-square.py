d722ff0f7c7e21780c4a86477f429164c8281358
ilyass <ilyass.bentalha2@gmail.com>
2023-10-24 17:48:58 +0100

    add task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""class square"""
 
 
 class Square:
+    """private instance and raise errors for exception"""
     def __init__(self, size=0):
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
