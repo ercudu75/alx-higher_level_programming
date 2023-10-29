7083afab2420df7dbf46f198773526de969c9503
ercudu <ilyass.bentalha2@gmail.com>
2023-10-29 15:17:55 +0000

    handle test cases

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(1e300**2, 1e300000))
+
 print(add_integer(-40))
 try:
     print(add_integer(4, "School"))
