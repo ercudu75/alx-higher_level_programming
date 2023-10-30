3de24e16c7c85ec1b9c7d9c6377f534fec761028
ercudu <ilyass.bentalha2@gmail.com>
2023-10-30 17:56:22 +0000

    fix code

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
         """add static method to check biggest area of Rectangle"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_1, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         area_1 = rect_1.area()
         area_2 = rect_2.area()
