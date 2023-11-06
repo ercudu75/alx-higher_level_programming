632993c9f0eada32dbbcf0ca935a32a3c6f04137
ercudu <ilyass.bentalha2@gmail.com>
2023-11-06 17:34:43 +0000

    add task advance 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""class MyInt"""
+
+
+class MyInt(int):
+    """invert equality and inequality"""
+    def __eq__(self, other):
+        """Invert the == operator's logic"""
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """Invert the != operator's logic"""
+        return int.__eq__(self, other)
