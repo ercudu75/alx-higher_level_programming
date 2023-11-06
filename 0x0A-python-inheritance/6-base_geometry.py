3b8edb87c4527cc2dfc1a11224919a35aaf90603
ercudu <ilyass.bentalha2@gmail.com>
2023-11-06 15:33:22 +0000

    add task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+
+    def __init__(self) -> None:
+        """init fucntion none"""
+        pass
+
+    def area(self):
+        """function that raise an error message """
+        raise Exception("area() is not implemented")
