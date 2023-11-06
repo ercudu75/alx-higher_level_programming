7494fbb1d9ebcc73d6388d281e4e95d13cba02e2
ercudu <ilyass.bentalha2@gmail.com>
2023-11-06 17:00:43 +0000

    add task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class that inherits from another class"""
+
+    def __init__(self, size):
+        """init file """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
