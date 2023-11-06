7b57f4bdafdefbee1868b61b8b7a97768b6e16e5
ercudu <ilyass.bentalha2@gmail.com>
2023-11-06 17:11:28 +0000

    add task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class that inherits from another class"""
+
+    def __init__(self, size):
+        """init file access to the parent class method"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return the square description"""
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
