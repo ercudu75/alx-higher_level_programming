53d2ff7d86dd75ae894d48b69043e37f4f9119ac
ercudu <ilyass.bentalha2@gmail.com>
2023-11-06 15:00:34 +0000

    add task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""fucntion check subclass"""
+
+
+def inherits_from(obj, a_class):
+    """sub class"""
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
