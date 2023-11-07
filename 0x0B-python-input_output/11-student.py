20b1c6f4dea77fd0dd8e019f30bfdb1b456b7859
ercudu <ilyass.bentalha2@gmail.com>
2023-11-07 18:20:51 +0000

    add task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """method in class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            new_dict = {}
+            for element in attrs:
+                if element in self.__dict__:
+                    new_dict[element] = self.__dict__[element]
+            return new_dict
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+
+    def reload_from_json(self, json):
+        """
+            Replaces all attributes of the Student instance
+            with values from json dictionary"""
+        for key, value in json.items():
+            setattr(self, key, value)
