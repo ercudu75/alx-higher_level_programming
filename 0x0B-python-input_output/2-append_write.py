c16e2cee3a54d63dcbdeedc7cd249a118e26613e
ercudu <ilyass.bentalha2@gmail.com>
2023-11-07 13:11:36 +0000

    add task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""fucntion append_write"""
+
+
+def append_write(filename="", text=""):
+    """appending to a file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
