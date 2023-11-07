9bd4c62dbe2853575fb35fc6691be831d67a45b5
ercudu <ilyass.bentalha2@gmail.com>
2023-11-07 14:41:07 +0000

    update pycodestyle and add comments

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""fucntion load_from_json_file"""
 import json
 
 
 def load_from_json_file(filename):
-	with open(filename, 'r', encoding='utf-8') as f:
-		return json.load(f)
+    """return Object from JSON"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
