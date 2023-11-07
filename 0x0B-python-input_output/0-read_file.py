ca87f6bc25a278fdfc0f374ee0c1e59767c38582
ercudu <ilyass.bentalha2@gmail.com>
2023-11-07 12:47:06 +0000

    add comment

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
-	with open(filename, 'r', encoding="utf-8") as f:
-		for line in f:
-			print(line, end="")
+    """read from file and print the text"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
