ad1bdec7fd3d90829cfffa90a1fb8db0a85c710d
ercudu <ilyass.bentalha2@gmail.com>
2023-11-07 16:56:55 +0000

    add task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """method in class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation"""
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
