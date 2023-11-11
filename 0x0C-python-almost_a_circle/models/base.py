e844657ac94e2377fac410518e88508c27157045
ercudu <ilyass.bentalha2@gmail.com>
2023-11-11 14:31:04 +0000

    add task 2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,13 +7,10 @@ class Base:
 
     __nb_objects = 0
 
-    def __init__(self, id = None):
+    def __init__(self, id=None):
         """Constructor function"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
-class Rectangle(Base)
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,53 @@
 #!/usr/bin/python3
+"""Class Rectangle"""
+from .base import Base
 
-from base import Base
+
+class Rectangle(Base):
+    """"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            constructor for the class Rectangle
+            and we call method form another class
+            we use super and pass the id The goal of it is
+            to manage id attribute in all our future classes
+            avoid duplicating the same code
+            """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def get_width(self):
+        return self.__width
+
+    @property
+    def get_height(self):
+        return self.__height
+
+    @property
+    def get_x(self):
+        return self.__x
+
+    @property
+    def get_y(self):
+        return self.__y
+
+    @get_width.setter
+    def setter_width(self, width):
+        self.__width = width
+
+    @get_height.setter
+    def setter_height(self, height):
+        self.__height = height
+
+    @get_x.setter
+    def setter_x(self, x):
+        self.__x = x
+
+    @get_y.setter
+    def setter_y(self, y):
+        self.__y = y
