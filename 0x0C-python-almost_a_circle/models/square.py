b9ca0f0e840753ffbec572cb0c46c3a67aaec28a
ilyass <ilyass.bentalha2@gmail.com>
2023-11-13 18:19:15 +0100

    alx fix this requirement of comments it should not break the checker the whole code compile perfect

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 """class Square"""
+
 from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Methods"""
+    """Representation of a Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """class constructor"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """custom __str__ method for Square"""
         return (f"[Square] ({self.id}) "
                 f"{self.x}/{self.y} - "
                 f"{self.width}")
 
     @property
     def size(self):
+        """size getter method"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """size setter method"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """updates values of the Square instance from args or kwargs"""
         if args and len(args) > 0:
             if len(args) > 0:
                 self.id = args[0]
@@ -38,6 +44,7 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """returns the dictionary representation of the Square instance"""
         return {
                 "id": self.id,
                 "size": self.width,
