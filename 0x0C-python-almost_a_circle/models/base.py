1dbff89a18294e2d316641c50fd11e9c37539ae9
ilyass <ilyass.bentalha2@gmail.com>
2023-11-13 18:11:40 +0100

    requirement that break the whole checker

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """class Base"""
+
 import json
 
 
 class Base:
-    """"""
+    """Representation of a Base"""
 
     __nb_objects = 0
 
@@ -18,12 +19,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
         list_dict = []
         if list_objs is not None:
             for obj in list_objs:
@@ -33,12 +36,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
         if json_string is None:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
         dummy = cls(1, 1)
         if dummy is not None:
             dummy.update(**dictionary)
@@ -46,6 +51,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
+        """returns a list of instances"""
         try:
             with open(cls.__name__ + '.json', 'r', encoding='utf-8') as f:
                 json_data = f.read()
