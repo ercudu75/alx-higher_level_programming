b6d41f49baec2e55e6dccfb9d4c7a133de41de81
ercudu <ilyass.bentalha2@gmail.com>
2024-02-14 19:16:31 +0000

    update task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- script that lists all genre and
 -- displays the number of shows linked to each
-SELECT G.name, COUNT(TG.genre_id) AS number_of_shows
+SELECT G.name AS genre, COUNT(TG.genre_id) AS number_of_shows
 FROM tv_genres G, tv_show_genres TG
 WHERE G.id = TG.genre_id
-GROUP BY G.name
+GROUP BY genre
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
