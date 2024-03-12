48b8773c20c5a8cc3e59efbd630f3c6f04f17727
ercudu <ilyass.bentalha2@gmail.com>
2024-03-12 02:26:37 +0000

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
-function add(a, b) {
-    let x = a + b;
-    console.log(x);
+function add (a, b) {
+  const x = a + b;
+  console.log(x);
 }
 
 add(a, b);
