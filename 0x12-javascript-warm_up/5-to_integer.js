9a457701de25c09eca00f3d7439a2a0d91a58f4d
ercudu <ilyass.bentalha2@gmail.com>
2024-03-12 01:38:15 +0000

    add tasks 0->6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+let first_arg = parseInt(process.argv[2]);
+if (Number.isInteger(first_arg)){
+	console.log('My number: ' + first_arg);
+}
+else {
+	console.log('Not a number');
+}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const arrstr = ['C is fun',
+  'Python is cool',
+  'JavaScript is amazing'];
+let str;
+
+for (str of arrstr) {
+  console.log(str);
+}
