24d43c517114a867a4bb86be3d0fbdb2da7b0884
ercudu <ilyass.bentalha2@gmail.com>
2024-03-12 23:50:13 +0000

    add tasks 6-7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let curr = 0;
+  for (i of list) {
+    if (i === searchElement) {
+      curr++;
+    }
+  }
+  return curr;
+}
