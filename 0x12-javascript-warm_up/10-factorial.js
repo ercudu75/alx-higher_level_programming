ef54214471bc6d08457bcddcb9af648428af0209
ercudu <ilyass.bentalha2@gmail.com>
2024-03-12 02:36:51 +0000

    add task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+function fact (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return n * fact(n - 1);
+}
+
+const m = parseInt(process.argv[2]);
+
+console.log(fact(m));
