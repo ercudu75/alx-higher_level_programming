bef57a467d71b1f34bf1c753e44cca7d491a7853
ercudu <ilyass.bentalha2@gmail.com>
2024-03-15 02:24:30 +0000

    add tasks 4->5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+import MySQLdb
+from sys import argv, exit
+
+
+try:
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                    FROM cities INNER JOIN states
+                    ON cities.state_id = states.id""")
+    rows = cur.fetchall()
+
+    for data in rows:
+        print(data)
+    cur.close()
+except MySQLdb.Error as e:
+    print("MySQL Error:", e)
+    exit(1)
+
+db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+import MySQLdb
+from sys import argv, exit
+
+
+try:
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("""SELECT cities.name
+                    FROM cities INNER JOIN states
+                    ON cities.state_id = states.id
+                    WHERE states.name = '{}'""".format(argv[4]))
+    rows = cur.fetchall()
+
+    cities = ", ".join(row[0] for row in rows)
+
+    print(cities)
+    cur.close()
+except MySQLdb.Error as e:
+    print("MySQL Error:", e)
+    exit(1)
+
+db.close()
