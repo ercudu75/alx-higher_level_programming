3ab79c8415051f0e6ca6c3a2860fa2a6caf78186
ercudu <ilyass.bentalha2@gmail.com>
2024-03-15 01:36:31 +0000

    add tasks 0->2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+import MySQLdb
+from sys import argv, exit
+
+
+try:
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states where name LIKE '{}'".format(argv[4]))
+    rows = cur.fetchall()
+
+    for data in rows:
+        print(data)
+    cur.close()
+except MySQLdb.Error as e:
+    print("MySQL Error:", e)
+    exit(1)
+
+db.close()
