e6af1f8c34c9e3ccd61fa9c39da0d766fdb86ca5
ercudu <ilyass.bentalha2@gmail.com>
2024-03-18 00:18:18 +0000

    add tasks 11->14

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""script to list all state objects using sqlalchemy
+"""
+from model_state import Base, State
+
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = session.query(State).filter(State.id == 2).one()
+    new_state.name = 'New Mexico'
+    session.commit()
